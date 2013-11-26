c93feed98260c4e42730f40c8f66e154cc729e32
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-11-26 16:11:13 +0100

    add 'Oprindelse'

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-﻿function addTemplateForm() {
-    $('#ticket-chat').append('<div class="select"><label for="eksempel">Eksempel</label><textarea id="eksempel" rows="8" size="100%" placeholder="Der skal være min. 1 eksempel i en Helpdesksag.                                                                                                                                                              Hvis der er tale om et testeksempel skal dette udførligt beskrives. " ></textarea></div>');
+﻿var textareaNewline = new Array(300).join(' ');
+
+function addTemplateForm() {
+    $('#ticket-chat').append('<div class="select"><label for="eksempel">Eksempel</label><textarea id="eksempel" rows="8" size="100%" placeholder="Der skal være min. 1 eksempel i en Helpdesksag.' + textareaNewline + 'Hvis der er tale om et testeksempel skal dette udførligt beskrives." ></textarea></div>');
+
+    $('#ticket-chat').append('<div class="select"><label for="oprindelse">Kilde/Oprindelse</label><textarea id="oprindelse" rows="8" size="100%" placeholder="Kunden har kontaktet KS' + textareaNewline + 'Rapport fra Backoffice (husk at skriv hvilken)' + textareaNewline + 'Tilfældigt opslag' + textareaNewline + 'Andet (f.eks. Test)" ></textarea></div>');
 }
 
 addTemplateForm();
