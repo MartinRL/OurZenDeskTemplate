1150a7aa8d72208dcc5aeac0011b0832863254da
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-11-26 16:00:30 +0100

    add 'eksempel'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-﻿function testManifest() {
-    console.log("hello template!");
+﻿function addTemplateForm() {
+    $('#ticket-chat').append('<div class="select"><label for="eksempel">Eksempel</label><textarea id="eksempel" rows="8" size="100%" placeholder="Der skal være min. 1 eksempel i en Helpdesksag.                                                                                                                                                              Hvis der er tale om et testeksempel skal dette udførligt beskrives. " ></textarea></div>');
 }
 
-testManifest();
+addTemplateForm();
